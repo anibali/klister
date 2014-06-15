d47c6e94138cb3dd9c669c7ef6bd3077b54112bc
Aiden Nibali <dismaldenizen@gmail.com>
2014-06-15 15:16:27 +1000

    Changed Main class for easier benchmarking

--- src/main/scala/org.nibali.klister/Main.scala
+++ src/main/scala/org.nibali.klister/Main.scala
@@ -5,6 +5,7 @@ import org.nibali.klister.Klister._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 
 object Main extends App {
   // Create Spark configuration
@@ -19,15 +20,34 @@ object Main extends App {
     sc.textFile(file).map(x => x.split("\\W+").map(_.toInt)).map(a => (a(0), a(1)))
   })
   val nReducers = args(2).toInt
-  val outFile = args(3)
 
-  val joined = rdds(0).inequalityJoin(rdds(1), Comparison.Less, nReducers)
-  //val joined = rdds(0).cartesian(rdds(1)).filter(x => x._1._1 < x._2._1)
-  //val joined = rdds(0).thetaJoin(rdds(1), _ < _, nReducers)
+  var joined:RDD[((Int, Int),(Int, Int))] = null
+  args(3) match {
+    case "theta" =>
+      joined = rdds(0).thetaJoin(rdds(1), _ < _, nReducers)
+    case "cartesian" =>
+      joined = rdds(0).cartesian(rdds(1)).filter(x => x._1._1 < x._2._1)
+    case "inequality" =>
+      joined = rdds(0).inequalityJoin(rdds(1), Comparison.Less, nReducers)
+    case _ =>
+      println("Valid execution types: theta, cartesian, inequality")
+  }
 
-  joined.saveAsTextFile(outFile)
+  if(joined != null)
+  {
+    println("Output contains " + joined.count() + " records")
 
-  println("Output contains " + joined.count() + " records")
+    println("Reducer output record counts:")
+
+    joined.mapPartitions(iter => {
+      var cnt = 0
+      while (iter.hasNext) {
+        iter.next
+        cnt += 1
+      }
+      List(cnt).iterator
+    }).collect().foreach(println)
+  }
 
   // Stop Spark context
   sc.stop()
