b66bd3c615b9177c6265ea70dfe45f5a6bb8d9ca
Aiden Nibali <dismaldenizen@gmail.com>
2014-08-09 14:00:14 +1000

    Improved similarity code

--- src/main/scala/org.nibali.klister/Similarity.scala
+++ src/main/scala/org.nibali.klister/Similarity.scala
@@ -4,91 +4,26 @@ import scala.collection.mutable
 
 object Similarity
 {
-  def test(shingleSize:Int = 5) {
-    val matrix = new CharacteristicMatrix()
-    val docs = List(
-      "Hello there Rob",
-      "Hello there Bob",
-      "I want to rob a bank",
-      "Cheese",
-      "Hello there Rob"
-    )
-    var i = 0
-    docs.foreach(doc => {
-      matrix.add(hashedShingles(doc, shingleSize), i)
-      i += 1
-    })
-    // Compute signature matrix
-    val perms = 1009 // Current hash functions require this to be prime
-    val arr = Array.fill(perms, docs.size) {Int.MaxValue}
-
-    val rowLabels = matrix.rowLabels.toArray
-    Range(0, rowLabels.size).foreach(i => {
-      matrix.getRow(rowLabels(i)).foreach(col => {
-        Range(0, perms).foreach(seed => {
-          if(hash(seed, i, perms) < arr(seed)(col))
-            arr(seed)(col) = hash(seed, i, perms)
-        })
+  def minhashSignature(nHashes:Int, hashFn:(Int, Int) => Int, features:Set[Int]):Array[Int] = {
+    val sig = Array.fill(nHashes) {Int.MaxValue}
+    features.foreach(feature => {
+      Range(0, nHashes).foreach(i => {
+        sig(i) = math.min(sig(i), hashFn(i, feature))
       })
     })
-    
-    // Calculate approximate Jaccard similarities
-    for(x <- Range(0,docs.size); y <- Range(0,docs.size)) {
-      if(x < y) {
-        println(docs(x), docs(y))
-        var matches = 0f
-        Range(0, perms).foreach(i => {
-          if(arr(i)(x) == arr(i)(y)) {
-            matches += 1
-          }
-        })
-        // Approximate similarity
-        println("%.2f%%".format(100 * matches / perms))
-      }
-    }
+    return sig
   }
 
-  def hash(index:Int, x:Int, mod:Int):Int = {
-    if(index == 0) {
-      return x
-    } else {
-      val rand = new scala.util.Random(index)
-      return ((x * (index * 2) + rand.nextInt) % mod + mod) % mod
-    }
+  def universalHash(index:Int, x:Int):Int = {
+    val rand = new scala.util.Random(index)
+    val a = rand.nextInt(Int.MaxValue - 1) + 1
+    val b = rand.nextInt(Int.MaxValue - 1) + 1
+    // Note: Conversion to int is same as doing 'mod <prime>' because
+    // 2^31 - 1 is prime (yay)
+    return (a * x + b).toInt
   }
 
   def hashedShingles(doc:String, k:Int = 9):Set[Int] = {
     doc.toLowerCase.sliding(k).map(_.hashCode).toSet
-  }
-
-  class CharacteristicMatrix {
-    private val map = mutable.Map[Int, mutable.Set[Int]]()
-    private val cols = mutable.Set[Int]()
-
-    def add(rows:Set[Int], col:Int) {
-      rows.foreach(row => {
-        if(map.contains(row))
-          map(row).add(col)
-        else
-          map(row) = mutable.Set[Int](col)
-      })
-      cols.add(col)
-    }
-
-    def rowLabels:Iterable[Int] = {
-      return map.keys
-    }
-
-    def colLabels:Iterable[Int] = {
-      return cols
-    }
-
-    def getRow(row:Int):mutable.Set[Int] = {
-      return map(row)
-    }
-
-    def contains(row:Int, col:Int):Boolean = {
-      return map(row).contains(col)
-    }
   }
 }
---
+++ src/test/scala/SimilaritySuite.scala
@@ -0,0 +1,39 @@
+package org.nibali.klister
+
+import org.nibali.klister.Klister._
+
+import org.scalatest.FunSuite
+import org.scalautils.Tolerance._
+ 
+class SimilaritySuite extends FunSuite {
+  test("minhashSignature") {
+    val shingleSize = 3
+    val nHashes = 100
+
+    val docs = List(
+      "Hello there Rob",
+      "Hello there Bob",
+      "I want to rob a bank",
+      "Cheese",
+      "Hello there Rob"
+    )
+    val shingled = docs.map(Similarity.hashedShingles(_, shingleSize))
+    val sigs = shingled.map(Similarity.minhashSignature(nHashes, Similarity.universalHash, _))
+
+    for(x <- Range(0,docs.size); y <- Range(0,docs.size)) {
+      if(x < y) {
+        var matches = 0f
+        Range(0, nHashes).foreach(i => {
+          if(sigs(x)(i) == sigs(y)(i)) {
+            matches += 1
+          }
+        })
+        // Estimated Jaccard similarity
+        val approx = matches / nHashes
+        // Actual Jaccard similarity
+        val exact = shingled(x).intersect(shingled(y)).size / shingled(x).union(shingled(y)).size.toFloat
+        assert(approx === (exact +- 0.05f))
+      }
+    }
+  }
+}
